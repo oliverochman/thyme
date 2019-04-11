dc48bbefe47d6ca5ab84fcf9b31be4c43e909acc
nevroje <joakim.berglind@gmail.com>
2019-04-11 11:33:00 +0200

    added line chart in chart component

--- src/components/DashboardChart.js
+++ src/components/DashboardChart.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
+import { Line } from 'react-chartjs-2';
 
 class DashboardChart extends Component {
 	render() {
+		const data = {
+			labels: [
+        '10/04/2018', '10/05/2018',
+        '10/06/2018', '10/07/2018',
+        '10/08/2018', '10/09/2018',
+        '10/10/2018', '10/11/2018',
+        '10/12/2018', '10/13/2018',
+        '10/14/2018', '10/15/2018'
+			],
+			datasets: [
+        {
+          label: 'Temperature',
+          data: [22,19,27,23,22,24,17,25,23,24,20,19],
+          fill: false,
+          borderColor: 'green'
+        }
+      ]
+		}
+
+		const options = {
+      maintainAspectRatio: false
+		}
+
 		return (
 			<div>
 				<h1>It works</h1>
+				<div className="chart-data">
+				<Line data={data} options={options}/>
+				</div>
 			</div>
 		);
 	}
