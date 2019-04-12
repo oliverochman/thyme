5abab8e56621cd55da8771e44fce6d74899b1063
oliverochman <oliver.ochman@hotmail.com>
2019-04-12 15:01:49 +0200

    formating data for chart

--- src/components/DashboardChart.js
+++ src/components/DashboardChart.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Bar } from "react-chartjs-2";
+import { Bar, Doughnut } from "react-chartjs-2";
 import axios from "axios";
 import { getProjects } from "../modules/getProjects";
 import { getTimesheets } from "../modules/getData";
+import { Dropdown } from "semantic-ui-react";
+
 
 
 class DashboardChart extends Component {
@@ -15,25 +17,48 @@ class DashboardChart extends Component {
 			activities: []
 		};
 	}
-	async componentDidMount() {
+
+	async doughnutLogic(month) {
 		const projectsResponse = await getProjects();
 		const timesheetsResponse = await getTimesheets();
 
 		const projects = projectsResponse.data;
 		const timesheets = timesheetsResponse.data;
-		debugger;
 
-		timesheets= timesheets.map(time => {
-			projects.filter(pro => {
-				if (time.project === pro.id) {
-					time.project = pro.name
-				}
-			})
+		const filtredTimesheets = timesheets.filter(asd => asd.begin > month).map(time => {
+			let index = projects.findIndex(project => project.id === time.project)
+			let project = projects[index]
+			time.project = project.name
 			return time
 		})
 
-		let projectsDuration = []
+		const onlyProjectNames = filtredTimesheets.map(val => {
+			return {projectName: val.project}
+		})
+
+		const uniqueProjects = onlyProjectNames.filter((project,index) => {
+			return index === onlyProjectNames.findIndex(obj => {
+				return JSON.stringify(obj) === JSON.stringify(project);
+			});
+		});
+
+		timesheets.map(time => {
+			uniqueProjects.map(project => {
+				if (project.projectName === time.project) {
+					if (project.duration) {
+						project.duration = project.duration + time.duration
+								} else {
+						project.duration = time.duration
+					}
+				}
+			})
+		})
+
 		debugger;
+	}
+
+	componentDidMount() {
+		this.doughnutLogic("2019-04-01 00:00")
 
 		// let projectNames = [];
 		// projects.forEach(element => {
@@ -61,9 +86,32 @@ class DashboardChart extends Component {
 	}
 
 	render() {
+		const months = [
+			{text: "January", value: "2019-01-01 00:00"},
+			{text: "February", value: "2019-02-01 00:00"},
+			{text: "March", value: "2019-03-01 00:00"},
+			{text: "April", value: "2019-04-01 00:00"},
+			{text: "May", value: "2018-05-01 00:00"},
+			{text: "June", value: "2018-06-01 00:00"},
+			{text: "July", value: "2018-07-01 00:00"},
+			{text: "August", value: "2018-08-01 00:00"},
+			{text: "September", value: "2018-09-01 00:00"},
+			{text: "October", value: "2018-10-01 00:00"},
+			{text: "November", value: "2018-11-01 00:00"},
+			{text: "December", value: "2018-12-01 00:00"}
+		]
+
 		return (
 			<div>
 				<h1>Dashboard </h1>
+				<Dropdown
+					options={months}
+					placeholder="Customer"
+					id="customer"
+					selection
+					onChange={(e, { value }) => this.doughnutLogic(value)}
+				>
+				</Dropdown>
 				<div className="chart-data">
 					<Bar
 						data={this.state.Data}
@@ -76,12 +124,3 @@ class DashboardChart extends Component {
 }
 
 export default DashboardChart;
-
-
-// filter projects to get the only ones that are listed in the collection of timesheets
-
-// then we have projects that are only relavent for current user
-
-// then we want to get duration of each timesheet containing the project id
-
-// where project name is the same, we want to add their durations together the
